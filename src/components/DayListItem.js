65410abd0c9e0cb32298021b1f3687a61f40b364
JimmyLin39 <jimmylin0309@email.com>
2019-10-06 11:40:32 -0700

    add interviewer components

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import './DayListItem.scss'
 import classNames from 'classnames'
+import './DayListItem.scss'
 
 export default function DayListItem(props) {
-  const dayListItemClassName = classNames({
+  const itemClass = classNames({
     'day-list__item': true,
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
@@ -20,10 +20,7 @@ export default function DayListItem(props) {
     return spotsMessage
   }
   return (
-    <li
-      className={dayListItemClassName}
-      onClick={() => props.setDay(props.name)}
-    >
+    <li className={itemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
