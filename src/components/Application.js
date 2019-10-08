75d3a7c8755db6c4eb59b4efb00f33fc20c2170f
JimmyLin39 <jimmylin0309@email.com>
2019-10-07 20:43:15 -0700

    get appointments data

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,62 +4,32 @@ import axios from 'axios'
 import 'components/Application.scss'
 import DayList from 'components/DayList'
 import Appointment from 'components/Appointment'
+import { getAppointmentsForDay } from 'helpers/selectors'
 
-const appointments = [
-  {
-    id: 1,
-    time: '12pm'
-  },
-  {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 1,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png'
-      }
-    }
-  },
-  {
-    id: 3,
-    time: '2pm'
-  },
-  {
-    id: 4,
-    time: '3pm',
-    interview: {
-      student: 'Archie Cohen',
-      interviewer: {
-        id: 3,
-        name: 'Mildred Nazir',
-        avatar: 'https://i.imgur.com/T2WwVfS.png'
-      }
-    }
-  },
-  {
-    id: 5,
-    time: '4pm',
-    interview: {
-      student: 'Maria Boucher',
-      interviewer: {
-        id: 4,
-        name: 'Cohana Roy',
-        avatar: 'https://i.imgur.com/FK8V841.jpg'
-      }
-    }
-  },
-  { id: 'last', time: '5pm' }
-]
 export default function Application(props) {
-  const [day, setDay] = useState('Monday')
-  const [days, setDays] = useState([])
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: []
+  })
+  const { day, days, appointments } = state
+  const setDay = day => setState({ ...state, day })
+
   useEffect(() => {
-    axios.get('/api/days').then(response => {
-      setDays(response.data)
+    Promise.all([
+      Promise.resolve(axios.get('/api/days')),
+      Promise.resolve(axios.get('/api/appointments'))
+    ]).then(all => {
+      const days = all[0].data
+      const appointments = all[1].data
+      console.log(appointments)
+      setState(prev => ({
+        ...prev,
+        days,
+        appointments: getAppointmentsForDay({ days, appointments }, day)
+      }))
     })
-  }, [])
+  }, [day])
   return (
     <main className="layout">
       <section className="sidebar">
