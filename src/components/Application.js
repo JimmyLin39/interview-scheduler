a04cf614627ab94d92862c43e333d97c1a6df2e0
JimmyLin39 <jimmylin0309@email.com>
2019-10-10 21:47:50 -0700

    add useReduce

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 import 'components/Application.scss'
 import DayList from 'components/DayList'
@@ -9,56 +8,12 @@ import {
   getInterview,
   getInterviewersForDay
 } from 'helpers/selectors'
+import useApplicationData from 'hooks/useApplicationData'
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData()
   const { day, days } = state
-  const setDay = day => setState({ ...state, day })
 
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments')),
-      Promise.resolve(axios.get('/api/interviewers'))
-    ]).then(all => {
-      const days = all[0].data
-      const appointments = all[1].data
-      const interviewers = all[2].data
-      setState(prev => ({
-        ...prev,
-        days,
-        appointments,
-        interviewers
-      }))
-    })
-  }, [])
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-    setState({ ...state, appointments })
-  }
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-    setState({ ...state, appointments })
-  }
   const appointments = getAppointmentsForDay(state, day)
   const interviewers = getInterviewersForDay(state, day)
   const schedule = appointments.map(appointment => {
