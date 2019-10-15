83cc73edab8a8eb261c18a3710ea085fd5dec57e
Jimmy Lin <jlin@scopemedia.com>
2019-10-15 16:15:05 -0700

    add tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,7 +20,11 @@ export default function DayListItem(props) {
     return spotsMessage
   }
   return (
-    <li className={itemClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={itemClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
