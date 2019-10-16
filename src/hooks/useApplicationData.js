d8507c5a8230daa555ac8550552b6ed9d4f759b2
Jimmy Lin <jlin@scopemedia.com>
2019-10-16 15:34:14 -0700

    move reducer to seperate file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,56 +1,12 @@
 import { useReducer, useEffect } from 'react'
 import axios from 'axios'
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from 'reducers/application'
 
 export default function useApplicationData() {
-  const SET_DAY = 'SET_DAY'
-  const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA'
-  const SET_INTERVIEW = 'SET_INTERVIEW'
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case SET_DAY: {
-        state = { ...state, day: action.day }
-        return state
-      }
-      case SET_APPLICATION_DATA: {
-        const { days, appointments, interviewers } = action
-        state = { ...state, days, appointments, interviewers }
-        return state
-      }
-      case SET_INTERVIEW: {
-        const { id, interview } = action
-        const appointment = {
-          ...state.appointments[action.id],
-          interview
-        }
-        const appointments = {
-          ...state.appointments,
-          [id]: appointment
-        }
-        // update remaining spots
-        let days = [...state.days]
-        days = days.map(day => {
-          // skip
-          if (
-            (state.appointments[id].interview !== null && interview !== null) ||
-            !day.appointments.includes(id)
-          ) {
-            return day
-          }
-          return {
-            ...day,
-            spots: interview === null ? day.spots + 1 : day.spots - 1
-          }
-        })
-        state = { ...state, appointments, days }
-        return state
-      }
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        )
-    }
-  }
   const [state, dispatch] = useReducer(reducer, {
     day: 'Monday',
     days: [],
